0ad62bd4d214289a0545d482ae8d627d726f26c9
Jakub Hajduk <devhajduk@gmail.com>
2025-10-23 10:10:56 +0200

    fix(color-module): fix color value

--- src/value-resolvers.ts
+++ src/value-resolvers.ts
@@ -1,4 +1,4 @@
-import type { ColorTokenValue, DimensionTokenValue } from './dtcg-types';
+import type { DimensionTokenValue } from './dtcg-types';
 import { isRGBA } from './is-rgba';
 import { isVariableAlias } from './is-variable-alias';
 import { rgbaToHexa } from './rgba-to-hexa';
@@ -6,7 +6,12 @@ import type { ValueResolverFn } from './types';
 
 export const colorValueResolver: ValueResolverFn = (type, value) =>
 	type === 'color' && isRGBA(value)
-		? (rgbaToHexa(value as RGBA) as ColorTokenValue)
+		? {
+        colorSpace: 'srgb',
+        components: [value.r, value.g, value.b],
+        alpha: 'a' in value ? value.a : 1,
+        hex: rgbaToHexa({ r: value.r, g: value.g, b: value.b })
+    }
 		: undefined;
 
 export const dimensionValueResolver: ValueResolverFn = (type, value) =>
